b308e32fa33d7dca9ae6cc1bb15e21a8daf4a9c7
kassi-bertrand <bertran.billy@gmail.com>
2024-08-12 14:51:55 -0500

    chore(format): Formatted files correctly

--- frontend/lib/actions.ts
+++ frontend/lib/actions.ts
@@ -1,25 +1,25 @@
-"use server"
+"use server";
 
-import { revalidatePath } from "next/cache"
+import { revalidatePath } from "next/cache";
 
 export async function createPlayground(body: {
-    name: string
-    description: string
-    visibility: string
-    language: string
-    userId: string
-  }) {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_DATABASE_WORKER_URL}/api/playground`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${process.env.NEXT_PUBLIC_WORKERS_KEY}`,
-        },
-        body: JSON.stringify(body),
-      }
-    )
-  
-    return await res.text()
-  }
+  name: string;
+  description: string;
+  visibility: string;
+  language: string;
+  userId: string;
+}) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_DATABASE_WORKER_URL}/api/playground`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${process.env.NEXT_PUBLIC_WORKERS_KEY}`,
+      },
+      body: JSON.stringify(body),
+    }
+  );
+
+  return await res.text();
+}
